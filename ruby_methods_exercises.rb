7984151ffc2980c9e66c46a4a578bbb9d72ce17d
Hanjun Chen <hanjunchen@Hanjuns-MacBook-Air.local>
2018-08-06 20:18:25 -0500

    test1

---
+++ ruby_methods_exercises.rb
@@ -0,0 +1,59 @@
+# def add_it_up
+# 	sum = 3 + 5
+# 	puts sum
+# end
+
+# add_it_up()
+
+# puts 'what is your name?'
+# name = gets.chomp
+
+# def say_hello(x)
+# 	puts "hi my name is " + x
+
+# end
+
+# say_hello(name)
+
+# puts "what is your weight in lbs?"
+# pounds = gets.chomp.to_i
+
+# def weightConversion(x)
+# 	kilo = x / 2.2
+# 	puts kilo
+# end
+
+# weightConversion(pounds)
+
+
+# string = gets.chomp
+# array = string.split
+
+# def reverse(y)
+# 	until y.length = 0
+
+# 	end
+# end
+
+# array = ["cat", "dog", "sheep"]
+
+# array.each do |x|
+
+# end
+
+
+
+# puts 'enter num'
+# x = gets.chomp.to_i
+# puts 'enter another num'
+# y = gets.chomp.to_i
+
+# def division(a, b)
+# 	result = a / b
+# 	remainder = a % b
+# 	puts "result is " + result.to_s + " with remainder of " + remainder.to_s
+# end
+
+# division(x, y)
+
+
